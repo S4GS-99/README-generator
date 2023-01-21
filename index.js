34322843d96f44de60e1a5415d454452ed2bd9f7
Santiago <sagos0919@gmail.com>
2023-01-21 17:37:32 +0000

    markdown-generattor.js and index.js creates a basic README file

---
+++ index.js
@@ -0,0 +1,101 @@
+const fs = require('fs');
+const util = require('util');
+const path = require('path');
+const writeFileAsync = util.promisify(fs.writeFile);
+const inquirer = require('inquirer');
+const markdown_generator = require('./utils/markdown-generator.js');
+const generateMarkdownWith = markdown_generator.generateMarkdownWith;
+
+// array of questions for user
+const questions = [
+	{
+		type: 'input',
+		name: 'repo',
+		message: `What's the name of your repo?`,
+		default: 'README-generator',
+	},
+	{
+		type: 'input',
+		name: 'title',
+		message: `What's the title of your project?`,
+		default: 'Project Title',
+	},
+	{
+		type: 'input',
+		name: 'description',
+		message: `Please, give a short description of your program`,
+		default: 'Project Description',
+	},
+	{
+		type: 'input',
+		name: 'installation',
+		message: `How can others install your program?`,
+		default: 'Do not apply for installation',
+	},
+	{
+		type: 'input',
+		name: 'usage',
+		message: `How can others use yor program? `,
+	},
+	{
+		type: 'list',
+		name: 'license',
+		message: `What license you want to register?`,
+		choices: ['MIT', 'GNU GPLv3', 'ISC', 'Apache 2.0'],
+		default: 'MIT',
+	},
+	{
+		type: 'input',
+		name: 'contributing',
+		message: `Please, list your collaborators (GitHub profiles), third-party assets, and or tutorials`,
+	},
+	{
+		type: 'input',
+		name: 'tests',
+		message: `Do you want to add any test?`,
+		default: 'Do not apply for tests',
+	},
+	{
+		type: 'input',
+		name: 'github',
+		message: `what's your GitHub user name?`,
+		default: '2G2-99',
+	},
+	{
+		type: 'input',
+		name: 'email',
+		message: `What email can the users use to contact you?`,
+		default: 'random@gmail.com',
+	},
+];
+
+const prompt = inquirer.createPromptModule();
+
+// function to initialize program
+function init() {
+	const README_file = path.normalize('./assets/markdown/generatedREADME.md');
+
+	prompt(questions).then(data => {
+		fs.writeFile(README_file, generateMarkdownWith(data), err => {
+			err
+				? console.error('ERROR!')
+				: console.log('README successfully created!');
+		});
+	});
+}
+
+// ! NEED TO MAKE IT LIKE THIS
+// async function init() {
+// 	try {
+// 		await prompt;
+// 		const generateREADME = generateMarkdownWith(prompt);
+
+// 		await writeFileAsync('./utils/README.md', generateREADME);
+// 		console.log('Successfully wrote to README.md');
+// 	} catch (err) {
+// 		console.error('ERROR!');
+// 	}
+// }
+// ! ------------------------
+
+init();
---
+++ utils/markdown-generator.js
@@ -0,0 +1,68 @@
+function generateMarkdownWith(data) {
+	const draft = `
+# ${data.title}
+
+${checkLicense(data.license)}
+
+## Description
+
+${data.description}
+
+## Table of contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits}](#credits)
+- [License](#license)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+##Credits
+
+${data.contributing}
+
+${data.github}
+
+## License
+
+${data.license}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+In case you need to reach me or have any questions, here is my email ${
+		data.email
+	}
+
+`;
+
+	return draft;
+}
+
+function checkLicense(data) {
+	const license =
+		data === 'MIT'
+			? '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+			: data === 'GNU GPLv3'
+			? '[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-green.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+			: data === 'ISC'
+			? '[![License: ISC](https://img.shields.io/badge/License-ISC-orange.svg)](https://opensource.org/licenses/MIT)'
+			: data === 'Apache 2.0'
+			? '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+			: 'No License';
+
+	return license;
+}
+
+module.exports = {
+	generateMarkdownWith,
+	checkLicense,
+};
